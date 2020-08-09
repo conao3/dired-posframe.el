dea159afdae786103ad1d819288ada5f73390596
conao3 <conao3@gmail.com>
2020-08-09 10:07:46 +0900

    remove useless indent

--- dired-posframe-tests.el
+++ dired-posframe-tests.el
@@ -1,34 +1,34 @@
-  ;;; dired-posframe-tests.el --- Test definitions for dired-posframe  -*- lexical-binding: t; -*-
+;;; dired-posframe-tests.el --- Test definitions for dired-posframe  -*- lexical-binding: t; -*-
 
-  ;; Copyright (C) 2019  Naoya Yamashita
+;; Copyright (C) 2019  Naoya Yamashita
 
-  ;; Author: Naoya Yamashita <conao3@gmail.com>
-  ;; URL: https://github.com/conao3/dired-posframe.el
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; URL: https://github.com/conao3/dired-posframe.el
 
-  ;; This program is free software: you can redistribute it and/or modify
-  ;; it under the terms of the GNU General Public License as published by
-  ;; the Free Software Foundation, either version 3 of the License, or
-  ;; (at your option) any later version.
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-  ;; This program is distributed in the hope that it will be useful,
-  ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-  ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  ;; GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-  ;; You should have received a copy of the GNU General Public License
-  ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-  ;;; Commentary:
+;;; Commentary:
 
-  ;; Test definitions for `dired-posframe'.
-
-
-  ;;; Code:
-
-  (require 'cort)
-  (require 'dired-posframe)
+;; Test definitions for `dired-posframe'.
 
 
-  ;; (provide 'dired-posframe-tests)
+;;; Code:
 
-  ;;; dired-posframe-tests.el ends here
+(require 'cort)
+(require 'dired-posframe)
+
+
+;; (provide 'dired-posframe-tests)
+
+;;; dired-posframe-tests.el ends here
