3b6611353c8f6a01f37ce4f64e983823b697c76c
conao3 <conao3@gmail.com>
2020-08-09 17:35:28 +0900

    show posframe after mouse click

--- dired-posframe.el
+++ dired-posframe.el
@@ -272,6 +272,7 @@ Features:
     (backward-char       . dired-posframe--advice-show)
     (dired-next-line     . dired-posframe--advice-show)
     (dired-previous-line . dired-posframe--advice-show)
+    (mouse-set-point     . dired-posframe--advice-show)
     (dired-find-file     . dired-posframe--advice-hide)
     (keyboard-quit       . dired-posframe--advice-hide))
   "Alist for dired-posframe advice.
