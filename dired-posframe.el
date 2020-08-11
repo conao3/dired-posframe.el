c19a73cfbd0e15c506b4e07e5d24217c98537525
conao3 <conao3@gmail.com>
2020-08-11 15:21:41 +0900

    add manual posframe description

--- dired-posframe.el
+++ dired-posframe.el
@@ -28,6 +28,9 @@
 ;; To use this package, simply add this to your init.el:
 ;;   (add-hook 'dired-mode-hook 'dired-posframe-mode)
 
+;; Or, you can use manual dired-posframe.  Hide posframe to type C-g.
+;;   (define-key global-map (kbd "C-*") 'dired-posframe-show)
+
 
 ;;; Code:
 
